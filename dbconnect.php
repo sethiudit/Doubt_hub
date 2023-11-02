0474629d9eafe9a644a736c14d4b02622320a897
Udit Sethi <sethiudit2000@gmail.com>
2023-11-03 01:21:36 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql309.infinityfree.com";
-$username = "if0_35012496";
-$password = "l2QSh8wU3YvFq";
-$database = "if0_35012496_doubt_hub";
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
 
 $conn = mysqli_connect($servername,$username,$password,$database);
 // echo"connect";
