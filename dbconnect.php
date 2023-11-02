5641a308dcc295d95c70c564150458826147c5b2
Udit Sethi <sethiudit2000@gmail.com>
2023-11-03 01:23:15 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@ $servername = "";
 $username = "";
 $password = "";
 $database = "";
-
+//connection details hidden for security purposes
 $conn = mysqli_connect($servername,$username,$password,$database);
 // echo"connect";
 
